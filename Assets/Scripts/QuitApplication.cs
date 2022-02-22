a19cb7c15f5b8d38a937dd6ee83381a6da47fa0c
ZachaRuba <zacharyjcanderson@gmail.com>
2022-02-22 15:31:07 -0800

    Added quit for application

--- Assets/Scripts/QuitApplication.cs
+++ Assets/Scripts/QuitApplication.cs
@@ -13,6 +13,6 @@ public class QuitApplication : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) Debug.Log("You quit the game!");//Application.Quit();   
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();   
     }
 }
